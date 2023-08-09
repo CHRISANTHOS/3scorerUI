b56b5c6b39f5d6653892602319541e45b85454fd
CHRISANTHOS <uakuchie@gmail.com>
2023-08-09 19:09:05 +0100

    Bug Fix: Correct http handshake termination with http overrides

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:scorer_uchenna/pages/soccer_home.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -5,9 +6,18 @@ import 'package:provider/provider.dart';
 import 'package:scorer_uchenna/view_model/auth_display_view_model.dart';
 
 
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
+
 void main() async{
   await dotenv.load(fileName: ".env");
   runApp(const MyApp());
+  HttpOverrides.global = MyHttpOverrides();
 }
 
 class MyApp extends StatelessWidget {
